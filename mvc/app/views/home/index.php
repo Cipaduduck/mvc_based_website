ba5efaae32e011cfcbe01bc2830cea205e8384a2
Cipaduduck <71691156+Cipaduduck@users.noreply.github.com>
2021-03-08 20:34:10 +0700

    Delete mvc directory

--- mvc/app/views/home/index.php
+++
@@ -1,8 +0,0 @@
-
-   <div class="jumbotron">
-  <h1 class="display-4">Welcome To My Galeri :)</h1>
-  <p class="lead">This is place how i am expressing myself</p>
-  <hr class="my-4">
-  <p>Feel free to stay in my galery , ENJOY!</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-</div>
